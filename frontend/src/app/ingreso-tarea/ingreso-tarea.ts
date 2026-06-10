4e3106141203dc0c4387c1ca14b97d9ce333f70a
LeonardoRegueira <leonardoregueira1991@gmail.com>
2026-06-09 22:03:31 -0300

    creacion de hijo -ingresoTarea-

---
+++ frontend/src/app/ingreso-tarea/ingreso-tarea.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-tarea',
+  imports: [],
+  templateUrl: './ingreso-tarea.html',
+  styleUrl: './ingreso-tarea.css',
+})
+export class IngresoTarea {
+
+}
