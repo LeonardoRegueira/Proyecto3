fb94f2ab81eadd4fe1f0f68b6daa20ec28b6ecf4
Mayra Mamani <may-code7@users.noreply.github.com>
2026-06-23 01:45:48 -0300

    corrigo arranque de frontend y actualizo

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms'; // <-- Sumamos esta por las dudas
-import { AppIngresoTareaComponent } from './ingreso-tarea/ingreso-tarea'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, AppIngresoTareaComponent],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
  })
--- frontend/src/app/componentes/historial/historial.ts
+++ frontend/src/app/componentes/historial/historial.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {Todos} from "./todos/todos";
-import {Pendientes} from "./pendientes/pendientes";
-import {Progreso} from "./progreso/progreso";
-import {Completos} from "./completos/completos";
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-historial',
   standalone: true,
-  imports: [RouterLink,RouterOutlet, CommonModule, RouterLinkActive, Todos],
+  imports: [RouterLink, RouterOutlet, CommonModule],
   templateUrl: './historial.html',
   styleUrl: './historial.css',
 })
